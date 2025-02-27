1f2c10ead3762717f6e043f2a21586b8fb8d73d5
iamrahulroyy <rahulroy.ac.in@gmail.com>
2025-02-27 21:36:50 +0530

    initial

--- main.py
+++ main.py
@@ -26,7 +26,7 @@ app.include_router(rsvp_router)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  
+    allow_origins=["http://localhost:3000","http://localhost:8000","http://127.0.0.1:8000","http://127.0.0.1:3000"],  
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
